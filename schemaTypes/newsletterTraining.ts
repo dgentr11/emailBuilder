ff05d27ebbc95c81ea48e9ca2913dcfba976e189
dgentr11 <dgentr11@utk.edu>
2026-01-09 09:46:17 -0500

    Update newsletterTraining.ts

--- schemaTypes/newsletterTraining.ts
+++ schemaTypes/newsletterTraining.ts
@@ -3,14 +3,9 @@ import { defineType, defineField } from 'sanity';
 
 export default defineType({
   name: 'newsletterTraining',
-  title: 'FS Training Newsletter',
+  title: 'Training Newsletter',
   type: 'document',
   fields: [
-    defineField({
-      name: 'publishDate',
-      title: 'Publish Date',
-      type: 'date'
-    }),
     defineField({
         name: 'title',
         title: 'Title',
@@ -18,50 +13,14 @@ export default defineType({
         description: 'Internal title for display in the Studio',
     }),
      defineField({
-        name: 'emailTitle',
-        title: 'Display Title',
-        type: 'string',
-        description: 'H1 title to be displayed on the email',
-    }),
-    defineField({
-      name: 'subtitle',
-      title: 'Subtitle',
-      type: 'string',
-    }),
-    defineField({
-      name: 'headerImage',
-      title: 'Header Image',
-      type: 'image',
-      options: { hotspot: true },
-      fields: [
-        defineField({ name: 'alt', type: 'string', title: 'Alt text' }),
-      ],
-    }),
-    defineField({
-      name: 'intro',
-      title: 'Intro',
-      type: 'array',
-      of: [{ type: 'block' }],
+      name: 'publishDate',
+      title: 'Publish Date',
+      type: 'date',
     }),
     defineField({
       name: 'sections',
       title: 'Sections',
       type: 'newsletterSections',
-    }),
-    defineField({
-      name: 'outro',
-      title: 'Outro',
-      type: 'array',
-      of: [{ type: 'block' }],
-    }),
-    defineField({
-      name: 'cta',
-      title: 'CTA',
-      type: 'object',
-      fields: [
-        defineField({ name: 'label', type: 'string', title: 'Button Text' }),
-        defineField({ name: 'href', type: 'url', title: 'Button Link' }),
-      ],
     }),
     defineField({
       name: 'status',
